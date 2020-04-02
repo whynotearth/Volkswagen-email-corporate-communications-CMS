dc804fe736184a4bc4bdccf5b9ceaa01827dd53e
Atharva Sharma <mailatharvasharma@gmail.com>
2020-04-02 15:30:48 +0530

    add: init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app" class="text-center">
+    <router-view />
+  </div>
+</template>
+
+<style>
+html,
+body {
+  scroll-behavior: smooth;
+}
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-4xl font-bold text-gray-500">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+};
+</script>
