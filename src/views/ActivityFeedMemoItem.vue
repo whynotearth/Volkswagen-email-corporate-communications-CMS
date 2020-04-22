6975289bb6cce98be68fca400aed67849a1eb765
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-04-22 20:56:29 +0430

    input text - add icon

--- src/views/ActivityFeedMemoItem.vue
+++ src/views/ActivityFeedMemoItem.vue
@@ -6,15 +6,31 @@
     <!-- tabs: opened/unread -->
     <!-- list of emails -->
 
-    <BaseProgressBar :label="`Open rate: 22%`" :progress="22" />
+    <div class="mb-4">
+      <BaseProgressBar :label="`Open rate: 22%`" :progress="22" />
+    </div>
+
+    <BaseTabs>
+      <BaseTab class="text-left" name="Services" :selected="true">
+        <span>What we do</span>
+      </BaseTab>
+      <BaseTab class="text-left" name="Pricing">
+        <span>How much we do it for</span>
+      </BaseTab>
+      <BaseTab class="text-left" name="About Us">
+        <span>Why we do it</span>
+      </BaseTab>
+    </BaseTabs>
   </div>
 </template>
 
 <script>
 import BaseProgressBar from '@/components/BaseProgressBar.vue';
+import BaseTabs from '@/components/BaseTabs.vue';
+import BaseTab from '@/components/BaseTab.vue';
 
 export default {
   name: 'ActivityFeedMemoItem',
-  components: { BaseProgressBar }
+  components: { BaseProgressBar, BaseTabs, BaseTab }
 };
 </script>
