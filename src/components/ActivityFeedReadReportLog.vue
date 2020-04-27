c09f445f32ace13fa60d683cfa8b6dffe333fdfa
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-04-27 14:01:21 +0430

    memo stats - style not recieved log

--- src/components/ActivityFeedReadReportLog.vue
+++ src/components/ActivityFeedReadReportLog.vue
@@ -1,16 +1,12 @@
 <template functional>
   <div>
     <div class="tg-body-mobile em-high mb-2">To: {{ props.email }}</div>
-    <div :class="{ 'text-error': !props.deliverDateTime }" class="tg-caption-mobile">
-      <span v-if="props.deliverDateTime" class="em-disabled">Recieved: {{ props.deliverDateTime }}</span>
-      <span v-else class="text-error em-high">Not recieved</span>
+    <div class="tg-caption-mobile em-disabled">
+      <span v-if="props.deliverDateTime">Recieved: {{ props.deliverDateTime }}</span>
+      <span v-else>Not recieved</span>
     </div>
 
-    <div
-      v-if="!props.hideOpened && props.deliverDateTime"
-      :class="{ 'text-error': !props.openDateTime }"
-      class="tg-caption-mobile em-disabled"
-    >
+    <div v-if="!props.hideOpened && props.deliverDateTime" class="tg-caption-mobile em-disabled">
       Opened: {{ props.openDateTime }}
     </div>
   </div>
