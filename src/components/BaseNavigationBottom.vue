69169b24a9c5660beb92d4fc8002226429f44092
Somayeh Ebrahimi <s.ebrahimi@farakav.com>
2020-04-28 20:40:02 +0430

    #81 inprogress nav bottom

--- src/components/BaseNavigationBottom.vue
+++ src/components/BaseNavigationBottom.vue
@@ -1,19 +1,36 @@
 <template>
   <nav class="flex w-full fixed bottom-0 bg-transparent">
-    <ul class="flex-auto text-left menu list-none bg-primary">
+    <ul class="flex-auto text-left menu list-none bg-primary px-2">
       <li class="text-white inline-block m-2 mb-1 text-xs">
-        <HomeIcon class="m-auto" />
-        Home
+        <router-link to="/">
+          <HomeIcon class="m-auto" />
+          Home
+        </router-link>
       </li>
       <li class="text-white inline-block m-2 mb-1 text-xs">
-        <MemoIcon class="m-auto" />
-        Memo Stats
+        <router-link to="/activity-feed/memos">
+          <EmailIcon class="m-auto" />
+          Memo Stats
+        </router-link>
       </li>
     </ul>
     <BottomNavIcon class="bottom-nav-icon"/>
-    <div class="flex content-center toggle-button bg-secondary">
-      <AddIcon class="m-auto" />
+    <div class="flex content-center toggle-button bg-secondary shadow-6dp"
+      @click="toggleMenu()">
+      <AddIcon class="m-auto ease-in-out" :class="{'rotate-45': isOpen}"/>
+      <ul v-if="isOpen" class="toggle-menu list-none">
+        <li class="flex whitespace-no-wrap text-white text-xs">
+          <router-link to="/memo/add" class="flex items-center">
+            Internal Memo
+            <div class="rounded-full h-10 w-10 flex items-center justify-center bg-secondary ml-2
+              shadow-6dp">
+              <MemoIcon class="m-auto"/>
+            </div>
+          </router-link>
+        </li>
+      </ul>
     </div>
+    <div v-if="isOpen" class="overlay"></div>
   </nav>
 </template>
 
@@ -27,7 +44,17 @@ import BottomNavIcon from '@/assets/bottom-nav.svg';
 export default {
   name: 'NavigationBottom',
   components: { AddIcon, HomeIcon, MemoIcon, EmailIcon, BottomNavIcon },
-  props: {}
+  props: {},
+  data () {
+    return {
+      isOpen: false
+    }
+  },
+  methods: {
+    toggleMenu () {
+      this.isOpen = !this.isOpen;
+    }
+  }
 };
 </script>
 
@@ -36,9 +63,6 @@ export default {
   position: relative;
   float: right;
 }
-.w-icon {
-  width: 18px;
-}
 .toggle-button {
   width: 56px;
   height: 56px;
@@ -46,5 +70,27 @@ export default {
   position: absolute;
   top: -30px;
   right: 16px;
+}
+.toggle-menu {
+  position: absolute;
+  top: -50px;
+  right: 10px;
+}
+.rotate-45 {
+  transform: rotate(45deg); 
+}
+.overlay {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  z-index: -1;
+  background: rgba(0, 0, 0, .54);
+}
+.router-link-exact-active {
+  color: #03B3F9; 
+}
+.router-link-exact-active svg path {
+  fill: #03B3F9!important;
 }
 </style>
