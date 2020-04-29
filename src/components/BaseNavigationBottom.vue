3c37ba4b8f925025cca865211eed716c8a26bba9
Somayeh Ebrahimi <s.ebrahimi@farakav.com>
2020-04-29 11:44:02 +0430

    #81 fix nav bottom on desktop

--- src/components/BaseNavigationBottom.vue
+++ src/components/BaseNavigationBottom.vue
@@ -1,37 +1,46 @@
 <template>
-  <nav class="flex w-full fixed bottom-0 bg-transparent">
-    <ul class="flex-auto text-left menu list-none bg-primary px-2">
-      <li class="text-white inline-block m-2 mb-1 text-xs">
-        <router-link to="/">
-          <HomeIcon class="m-auto" />
-          Home
-        </router-link>
-      </li>
-      <li class="text-white inline-block m-2 mb-1 text-xs">
-        <router-link to="/activity-feed/memos">
-          <EmailIcon class="m-auto" />
-          Memo Stats
-        </router-link>
-      </li>
-    </ul>
-    <BottomNavIcon class="bottom-nav-icon" />
-    <div class="flex content-center toggle-button bg-secondary shadow-6dp" @click="toggleMenu()">
-      <AddIcon class="m-auto ease-in-out" :class="{ 'rotate-45': isOpen }" />
-      <ul v-if="isOpen" class="toggle-menu list-none">
-        <li class="flex whitespace-no-wrap text-white text-xs">
-          <router-link to="/memo/add" class="flex items-center">
-            Internal Memo
-            <div
-              class="rounded-full h-10 w-10 flex items-center justify-center bg-secondary ml-2
-              shadow-6dp"
-            >
-              <MemoIcon class="m-auto" />
-            </div>
-          </router-link>
-        </li>
-      </ul>
+  <nav class="flex w-full fixed bottom-0 bg-transparent z-20">
+    <div class="flex flex-auto">
+      <div class=" bg-primary flex-1"></div>
+      <div class="container flex relative px-0">
+        <ul class="flex-auto text-left menu list-none bg-primary px-2 md:px-0">
+          <li class="text-white inline-block m-2 mb-1 text-xs">
+            <router-link class="router-link" to="/">
+              <HomeIcon class="m-auto" />
+              Home
+            </router-link>
+          </li>
+          <li class="text-white inline-block m-2 mb-1 text-xs">
+            <router-link class="router-link" to="/activity-feed/memos">
+              <EmailIcon class="m-auto" />
+              Memo Stats
+            </router-link>
+          </li>
+        </ul>
+        <img
+          class="bottom-nav-icon"
+          src="https://res.cloudinary.com/whynotearth/image/upload/v1588091363/Volkswagen/cms/Bottom_Nav_utgvw8.png"
+        />
+        <div class="flex content-center toggle-button bg-secondary shadow-6dp" @click="toggleMenu()">
+          <AddIcon class="m-auto ease-in-out" :class="{ 'rotate-45': isOpen }" />
+          <ul v-if="isOpen" class="toggle-menu list-none">
+            <li class="flex whitespace-no-wrap text-white text-xs">
+              <router-link to="/memo/add" class="flex items-center">
+                Internal Memo
+                <div
+                  class="rounded-full h-10 w-10 flex items-center justify-center bg-secondary ml-2
+                  shadow-6dp"
+                >
+                  <MemoIcon class="m-auto" />
+                </div>
+              </router-link>
+            </li>
+          </ul>
+        </div>
+        <div v-if="isOpen" class="overlay"></div>
+      </div>
+      <div class=" bg-primary flex-1"></div>
     </div>
-    <div v-if="isOpen" class="overlay"></div>
   </nav>
 </template>
 
@@ -40,11 +49,10 @@ import AddIcon from '@/assets/add.svg';
 import HomeIcon from '@/assets/home.svg';
 import MemoIcon from '@/assets/memo.svg';
 import EmailIcon from '@/assets/email.svg';
-import BottomNavIcon from '@/assets/bottom-nav.svg';
 
 export default {
   name: 'NavigationBottom',
-  components: { AddIcon, HomeIcon, MemoIcon, EmailIcon, BottomNavIcon },
+  components: { AddIcon, HomeIcon, MemoIcon, EmailIcon },
   props: {},
   data() {
     return {
@@ -85,13 +93,29 @@ export default {
   width: 100%;
   height: 100%;
   top: 0;
+  left: 0;
   z-index: -1;
   background: rgba(0, 0, 0, 0.54);
 }
 .router-link-exact-active {
-  color: #03b3f9;
+  color: #03b3f9 !important;
+  opacity: 1 !important;
 }
 .router-link-exact-active svg path {
   fill: #03b3f9 !important;
+  fill-opacity: 1 !important;
+}
+.router-link {
+  color: white;
+  opacity: 0.54;
+}
+.router-link svg path {
+  fill: white;
+  fill-opacity: 0.54;
+}
+@media (min-width: 928px) {
+  .container {
+    max-width: 880px;
+  }
 }
 </style>
