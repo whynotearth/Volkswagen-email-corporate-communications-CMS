4245a25cd34805fb31bac43bcfae7b2277429ad2
Somayeh Ebrahimi <s.ebrahimi@farakav.com>
2020-04-30 16:22:00 +0430

    #15 UI of Add Post, step 1

--- src/views/PostAdd.vue
+++ src/views/PostAdd.vue
@@ -1,81 +1,33 @@
 <template>
   <div>
-    <PageNavigation />
-    <div class="w-full max-w-4xl mx-auto">
-      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 my-4">
-        <div>
-          <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="PostAddHeadline">
-              Headline
-            </label>
-            <input
-              v-model="headline"
-              name="Headline"
-              required
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="PostAddHeadline"
-              type="text"
-              placeholder="Write a title"
-            />
-          </div>
-
-          <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="PostAddContent">
-              Content
-            </label>
-            <textarea
-              v-model="content"
-              rows="12"
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="PostAddContent"
-              type="text"
-              placeholder="Write post content"
-            />
-          </div>
-        </div>
-        <div class="flex items-center justify-between">
-          <button
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
-            @click="submit()"
-          >
-            Save
-          </button>
-        </div>
-      </form>
-    </div>
+    <StepperManager :step="currentStep" :steps="steps" :options="{ submitText: 'New Post', isLastStep: 1 === 3 }">
+      <div class="px-0 overflow-y-auto flex flex-col h-full narrow-scrollbars">
+        <PostAddStep1 v-if="currentStep === 1" />
+      </div>
+    </StepperManager>
   </div>
 </template>
 
 <script>
-import PageNavigation from '@/components/PageNavigation.vue';
+import StepperManager from '@/components/StepperManager.vue';
+import PostAddStep1 from '@/components/PostAddStep1.vue';
 
 export default {
-  components: { PageNavigation },
-  name: 'PostAddPage',
+  name: 'PostAdd',
+  components: { StepperManager, PostAddStep1 },
+  props: {
+    step: {
+      default: 1
+    }
+  },
   data() {
     return {
-      headline: '',
-      content: ''
+      steps: ['Choose Category', 'New Post', 'Schedule Post']
     };
   },
-  methods: {
-    submit() {
-      try {
-        this.$store.dispatch('post/addItem', {
-          body: {
-            categoryId: 29,
-            headline: 'some headline',
-            description: 'some description',
-            date: '2020-04-10T15:42:20.253Z',
-            price: 0,
-            eventDate: '2020-04-10T15:42:20.253Z'
-            // images: ['string']
-          }
-        });
-      } catch (error) {
-        console.log(error);
-      }
+  computed: {
+    currentStep() {
+      return parseInt(this.step);
     }
   }
 };
