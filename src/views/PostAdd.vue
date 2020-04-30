7f29893458cd548cc08cc983602a467b2801bbaa
Somayeh Ebrahimi <s.ebrahimi@farakav.com>
2020-04-30 16:35:49 +0430

    #15 remove extra parent div

--- src/views/PostAdd.vue
+++ src/views/PostAdd.vue
@@ -1,11 +1,9 @@
 <template>
-  <div>
-    <StepperManager :step="currentStep" :steps="steps" :options="{ submitText: 'New Post', isLastStep: 1 === 3 }">
-      <div class="px-0 overflow-y-auto flex flex-col h-full narrow-scrollbars">
-        <PostAddStep1 v-if="currentStep === 1" />
-      </div>
-    </StepperManager>
-  </div>
+  <StepperManager :step="currentStep" :steps="steps" :options="{ submitText: 'New Post', isLastStep: 1 === 3 }">
+    <div class="px-0 overflow-y-auto flex flex-col h-full narrow-scrollbars">
+      <PostAddStep1 v-if="currentStep === 1" />
+    </div>
+  </StepperManager>
 </template>
 
 <script>
