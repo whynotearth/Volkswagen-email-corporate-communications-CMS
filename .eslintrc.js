fab2a66d0a29d492461ee29fac8adac24eee946e
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-05-01 17:44:42 +0430

    update eslint indent for switchcase

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vue/no-unused-components': 'warn',
-    indent: ['warn', 2],
+    indent: ['warn', 2, { SwitchCase: 1 }],
     quotes: ['warn', 'single'],
     // TODO remove
     'no-useless-catch': 'off',
