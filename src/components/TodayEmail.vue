e354a5b91c17c80e1c8882a33a5267ff2409ff18
abdullahcalisir <abdullahcalisir12@gmail.com>
2020-05-01 18:27:20 +0300

    heading style added, text replaced with image

--- src/components/TodayEmail.vue
+++ src/components/TodayEmail.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="flex flex-col items-center text-center px-4 my-6">
     <div>
-      <h2>Hi Xavier, here's your Jumpstart email for the day.</h2>
+      <h2 class="tg-h2-mobile text-primary">Hi Xavier, here's your Jumpstart email for the day.</h2>
     </div>
     <div class="my-4 px-6">
-      Image Component will be replaced here
+      <img
+        src="https://user-images.githubusercontent.com/5694308/80701860-4a421500-8b0a-11ea-8d97-c3b0df4d83a7.png"
+        height="373"
+        width="264"
+      />
     </div>
     <div>
       <h2 class="tg-h2-mobile text-primary">Daily Email will automatically send in</h2>
