597d7300cb2b13ce892668593cb2ecaaf3231777
Atharva Sharma <mailatharvasharma@gmail.com>
2020-05-04 00:30:02 +0530

    feat: jumpstart, position change of getters, mutations, actions

--- src/store/modules/email.js
+++ src/store/modules/email.js
@@ -8,7 +8,33 @@ export default {
     email_date: null,
     schedule_time: null,
     preview_link: '',
-    postIds: []
+    postIds: [],
+    email_recipients: [],
+    response_message: {
+      type: '', // error, success
+      message: '',
+      class: '' // text-error text-success
+    }
+  },
+  getters: {
+    get_email_date: state => state.email_date,
+    get_schedule_time: state => state.schedule_time,
+    get_postIds: state => state.postIds,
+    get_preview_link: state => state.preview_link,
+    get_email_recipients: state => state.email_recipients,
+    get_response_message: state => state.response_message
+  },
+  actions: {
+    async create_jumpstart({ commit }, payload) {
+      await JumpStartService.jumpstart(payload.params);
+    },
+    clear_email_data({ commit }) {
+      commit('update_email_date', null);
+      commit('update_schedule_time', null);
+      commit('update_preview_link', '');
+      commit('update_postIds'); // Passing no payload just clears the postIds array
+      commit('update_email_recipients', []);
+    }
   },
   mutations: {
     update_email_date(state, payload) {
@@ -36,12 +62,12 @@ export default {
         url.searchParams.append('postIds', postId);
       });
       state.preview_link = url.href;
+    },
+    update_email_recipients(state, payload) {
+      state.email_recipients = payload;
+    },
+    update_response_message(state, payload) {
+      state.response_message = payload;
     }
-  },
-  getters: {
-    get_email_date: state => state.email_date,
-    get_schedule_time: state => state.schedule_time,
-    get_postIds: state => state.postIds,
-    get_preview_link: state => state.preview_link
   }
 };
