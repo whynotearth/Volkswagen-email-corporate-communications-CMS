cb9d0830921cb03f9b3047eabe94bed0e6ae08bd
Atharva Sharma <mailatharvasharma@gmail.com>
2020-05-04 13:05:48 +0530

    fix: timeslots from 800 to 2400

--- src/components/Email/ScheduleEmail.vue
+++ src/components/Email/ScheduleEmail.vue
@@ -73,7 +73,7 @@ export default {
       let time = [];
       if (this.get_email_date) {
         let d = new Date(this.get_email_date);
-        let start = 1000;
+        let start = 800;
         let end = 2400;
         let startHours = Math.floor(start / 100) * 3600000;
         let endHours = Math.floor(end / 100) * 3600000;
