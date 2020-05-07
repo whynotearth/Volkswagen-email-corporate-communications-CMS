5e9a14a46331015bb0432c05fb59a37fdac1b09e
Christopher Saburo <christopher.saburo@outlook.com>
2020-05-07 09:59:26 -0700

    recover maing page into correct one

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,20 +16,20 @@
                 alt=""
               />
             </div>
-            <h1 class="text-primary h1-mobile md:h1 mb-h1-mobile">Let's crunch the numbers...</h1>
+            <h1 class="text-primary h1-mobile md:h1 mb-h1-mobile">Welcome to Blue Delta</h1>
 
             <div v-if="isAuthenticated">
               <router-link
-                :to="{ name: 'ActivityFeedMemoList' }"
+                :to="{ name: 'MemoAdd' }"
                 class="block bg-secondary w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline transition duration-100 ease-in-out transition-all label-mobile mb-6"
               >
-                BLUE DELTA STATS
+                New Memo
               </router-link>
               <router-link
                 :to="{ name: 'ActivityFeedMemoList' }"
                 class="block bg-secondary w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline transition duration-100 ease-in-out transition-all label-mobile"
               >
-                MEMO STATS
+                View Stats
               </router-link>
             </div>
 
