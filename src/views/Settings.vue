e1bf9a77f35abd05f3f1fb8ac2a9ca6b1bf95d72
Christopher Saburo <christopher.saburo@outlook.com>
2020-05-07 23:54:49 -0700

    arranged items in setting page

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -6,10 +6,56 @@
         <div class="container px-0 md:px-6">
           <div class="flex items-strech items-center border-b-1 border-divider">
             <router-link
+              :to="{ name: '' }"
+              class="link-fordward block flex-grow justify-between flex h-full items-center cursor-pointer select-none px-4 pr-6 py-5"
+            >
+              <span class="mr-2 tg-body-mobile">My Account</span>
+              <svg
+                class="transform -scale-x-1 text-gray"
+                width="8"
+                height="12"
+                viewBox="0 0 8 12"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M7.4 1.4L6 0L0 6L6 12L7.4 10.6L2.8 6L7.4 1.4Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </router-link>
+          </div>
+          <div class="flex items-strech items-center border-b-1 border-divider">
+            <router-link
+              :to="{ name: '' }"
+              class="link-fordward block flex-grow justify-between flex h-full items-center cursor-pointer select-none px-4 pr-6 py-5"
+            >
+              <span class="mr-2 tg-body-mobile">Blue Delta Settings</span>
+              <svg
+                class="transform -scale-x-1 text-gray"
+                width="8"
+                height="12"
+                viewBox="0 0 8 12"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M7.4 1.4L6 0L0 6L6 12L7.4 10.6L2.8 6L7.4 1.4Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </router-link>
+          </div>
+          <div class="flex items-strech items-center border-b-1 border-divider">
+            <router-link
               :to="{ name: 'EmailLists' }"
               class="link-fordward block flex-grow justify-between flex h-full items-center cursor-pointer select-none px-4 pr-6 py-5"
             >
-              <span class="mr-2 tg-body-mobile">Email Settings</span>
+              <span class="mr-2 tg-body-mobile">Distribution Groups</span>
               <svg
                 class="transform -scale-x-1 text-gray"
                 width="8"
