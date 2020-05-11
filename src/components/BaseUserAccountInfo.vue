4edffa0368df2a0e9e0748f59a6005429d65e046
Christopher Saburo <christopher.saburo@outlook.com>
2020-05-11 01:51:38 -0700

    update store/auth store about name state

--- src/components/BaseUserAccountInfo.vue
+++ src/components/BaseUserAccountInfo.vue
@@ -6,10 +6,10 @@
       </h2>
     </div>
     <div class="mb-4">
-      <BaseInputText class="bg-surface" label="Name" type="text" v-model="username"></BaseInputText>
+      <BaseInputText class="bg-surface" label="Name" type="text" v-model="name"></BaseInputText>
     </div>
     <div class="mb-4">
-      <BaseInputText class="bg-surface" label="Email" type="email" v-model="password"></BaseInputText>
+      <BaseInputText class="bg-surface" label="Email" type="email" v-model="email"></BaseInputText>
     </div>
     <div class="mb-4 items-center flex justify-center py-8">
       <BaseButton>
@@ -20,6 +20,7 @@
 </template>
 
 <script>
+import store from '@/store';
 import { mapGetters } from 'vuex';
 import BaseInputText from '@/components/BaseInputText.vue';
 import BaseButton from '@/components/BaseButton.vue';
@@ -28,13 +29,7 @@ export default {
   name: 'BaseUserAccountInfo',
   components: { BaseInputText, BaseButton },
   computed: {
-    ...mapGetters('auth', ['email', 'password'])
-  },
-  data() {
-    return {
-      username: '',
-      password: ''
-    };
+    ...mapGetters('auth', ['name', 'email'])
   }
 };
 </script>
