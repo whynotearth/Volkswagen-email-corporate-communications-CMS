0153545453cafc693df4fc24b8064e1ff00daba7
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2020-05-11 17:51:32 +0300

    memo list item sytle edited

--- src/components/LayoutFixedScrollable.vue
+++ src/components/LayoutFixedScrollable.vue
@@ -2,7 +2,7 @@
   <div class="page h-full flex flex-col">
     <slot name="header" />
     <div class="flex-grow z-2 overflow-y-hidden">
-      <div class="px-0 overflow-y-auto flex flex-col h-full narrow-scrollbars">
+      <div class="px-0 overflow-y-auto overflow-x-hidden flex flex-col h-full narrow-scrollbars">
         <slot name="content" />
       </div>
     </div>
