05ddf16333cca64c29f88aee47d6772c598497ce
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-05-11 14:28:02 +0430

    use fixed scrollable layout in jumpstart-lists and article-list/item

--- src/views/JumpStartLists.vue
+++ src/views/JumpStartLists.vue
@@ -1,24 +1,29 @@
 <template>
-  <div class="">
-    <BaseAppBarHeader title="Blue Delta" :to-link="{ name: 'Dashboard' }" />
-    <div class="flex flex-col py-3">
-      <div class="container px-0 md:px-6">
-        <template v-for="(plan, index) in get_daily_plan">
-          <JumpStartItem :key="index" :model="plan" />
-        </template>
+  <LayoutFixedScrollable>
+    <template #header>
+      <BaseAppBarHeader title="Blue Delta" :to-link="{ name: 'Dashboard' }" />
+    </template>
+    <template #content>
+      <div class="flex flex-col py-3">
+        <div class="container px-0 md:px-6">
+          <template v-for="(plan, index) in get_daily_plan">
+            <JumpStartItem :key="index" :model="plan" />
+          </template>
+        </div>
       </div>
-    </div>
-  </div>
+    </template>
+  </LayoutFixedScrollable>
 </template>
 
 <script>
 import BaseAppBarHeader from '@/components/BaseAppBarHeader.vue';
 import JumpStartItem from '@/components/JumpStartListItem.vue';
 import { mapGetters, mapActions } from 'vuex';
+import LayoutFixedScrollable from '@/components/LayoutFixedScrollable.vue';
 
 export default {
   name: 'JumpStart',
-  components: { BaseAppBarHeader, JumpStartItem },
+  components: { BaseAppBarHeader, JumpStartItem, LayoutFixedScrollable },
   computed: {
     ...mapGetters('article', ['get_daily_plan'])
   },
