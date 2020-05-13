0f842ebaf33e1744287752ba1997a2cb8a2aeb4e
Christopher Saburo <christopher.saburo@outlook.com>
2020-05-12 20:38:52 -0700

    update issue

--- src/components/BaseUserAccountInfo.vue
+++ src/components/BaseUserAccountInfo.vue
@@ -6,10 +6,10 @@
       </h2>
     </div>
     <div class="mb-4">
-      <BaseInputText class="bg-surface" label="Name" type="text" v-model="name"></BaseInputText>
+      <BaseInputText class="bg-surface" label="Name" type="text" v-model="username"></BaseInputText>
     </div>
     <div class="mb-4">
-      <BaseInputText class="bg-surface" label="Email" type="email" v-model="email"></BaseInputText>
+      <BaseInputText class="bg-surface" label="Email" type="email" v-model="useremail"></BaseInputText>
     </div>
     <div class="mb-4 items-center flex justify-center py-8">
       <BaseButton>
@@ -21,15 +21,34 @@
 
 <script>
 import store from '@/store';
-import { mapGetters } from 'vuex';
+import { mapGetters, mapMutations } from 'vuex';
 import BaseInputText from '@/components/BaseInputText.vue';
 import BaseButton from '@/components/BaseButton.vue';
 
 export default {
   name: 'BaseUserAccountInfo',
   components: { BaseInputText, BaseButton },
+  methods: {
+    ...mapMutations('auth', ['update_name', 'update_email'])
+  },
   computed: {
-    ...mapGetters('auth', ['name', 'email'])
+    ...mapGetters('auth', ['name', 'email']),
+    username: {
+      get() {
+        return this.name;
+      },
+      set(value) {
+        this.update_name(value);
+      }
+    },
+    useremail: {
+      get() {
+        return this.email;
+      },
+      set(value) {
+        this.update_email(value);
+      }
+    }
   }
 };
 </script>
