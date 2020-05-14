75a8159e3ca0d76c233f23a584276d1be7ec0af4
Christopher Saburo <christopher.saburo@outlook.com>
2020-05-14 04:48:22 -0700

    complete spinner animation in memos page

--- src/views/ActivityFeedMemoList.vue
+++ src/views/ActivityFeedMemoList.vue
@@ -11,6 +11,9 @@
     </template>
 
     <template #content>
+      <div class="container py-4 flex justify-center" v-if="!is_fetch_memo">
+        <div class="loader"></div>
+      </div>
       <div class="container px-0 md:px-6 text-left">
         <div class="px-4 pt-4">
           <div class="mb-8" v-for="memoItem in get_memos" :key="memoItem.id">
@@ -48,8 +51,52 @@ export default {
   methods: {
     ...mapActions('memo', ['fetch_memos'])
   },
+  data() {
+    return {
+      is_fetch_memo: false
+    };
+  },
   mounted() {
-    this.fetch_memos();
+    this.is_fetch_memo = false;
+    try {
+      this.fetch_memos().then(() => {
+        this.is_fetch_memo = true;
+      });
+    } catch (error) {
+      console.log('fetch memos error');
+    }
   }
 };
 </script>
+
+<style>
+.loader {
+  border: 6px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 6px solid #011d51;
+  border-bottom: 6px solid #011d51;
+  width: 40px;
+  height: 40px;
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 2s linear infinite;
+}
+
+/* Safari */
+@-webkit-keyframes spin {
+  0% {
+    -webkit-transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
