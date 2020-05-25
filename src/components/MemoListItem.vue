a48569d8175515c7a37b2ee07005dba5f6befa42
Christopher Saburo <christopher.saburo@outlook.com>
2020-05-24 23:47:35 -0700

    added desktop sizing for memo stat

--- src/components/MemoListItem.vue
+++ src/components/MemoListItem.vue
@@ -3,7 +3,7 @@
     <!-- title and date -->
     <div class="">
       <h2
-        class="tg-body-emphasis-mobile capitalize em-high text-inherit flex-grow whitespace-no-wrap overflow-x-hidden truncate transition transition-colors duration-100"
+        class="md:tg-body-emphasis-desktop tg-body-emphasis-mobile capitalize em-high text-inherit flex-grow whitespace-no-wrap overflow-x-hidden truncate transition transition-colors duration-100"
         :title="model.subject"
       >
         {{ model.subject }}
@@ -18,24 +18,24 @@
     <!-- contents -->
     <div class="flex justify-between flex-no-wrap">
       <div class="">
-        <div class="tg-caption-mobile em-high text-black">To: {{ model.to }}</div>
+        <div class="md:tg-caption-desktop tg-caption-mobile em-high text-black">To: {{ model.to }}</div>
         <div class="flex items-center">
-          <div class="tg-caption-bold-mobile text-black whitespace-no-wrap">
+          <div class="md:tg-caption-bold-desktop tg-caption-bold-mobile text-black whitespace-no-wrap">
             {{ formatDate(model.creationDateTime, 'd MMM') }}
           </div>
           <div class="h-2 w-2 mx-2 rounded-full bg-secondary"></div>
           <div class="em-high text-black">
-            <span class="tg-caption-mobile">Open rate: </span>
-            <span class="tg-caption-bold-mobile">{{ model.openPercentage }}%</span>
+            <span class="md:tg-caption-desktop tg-caption-mobile">Open rate: </span>
+            <span class="md:tg-caption-bold-desktop tg-caption-bold-mobile">{{ model.openPercentage }}%</span>
           </div>
         </div>
         <div
-          class="memo-list-item--description mt-2 tg-caption-mobile em-disabled text-black whitespace-pre-line overflow-hidden"
+          class="memo-list-item--description mt-2 md:tg-caption-desktop tg-caption-mobile em-disabled text-black whitespace-pre-line overflow-hidden"
         >
           {{ model.description }}
         </div>
       </div>
-      <div class="w-20 flex-shrink-0">
+      <div class="w-20 md:w-32 flex-shrink-0">
         <img
           src="https://res.cloudinary.com/whynotearth/image/upload/v1588074840/Volkswagen/cms/Internal_Memo_kbc8ys.png"
           alt=""
