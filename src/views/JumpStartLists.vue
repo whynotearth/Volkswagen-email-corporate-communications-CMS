0f3fae7365f541640049ca7142e17f857046bdf2
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-06-04 12:56:01 +0430

    jumpstart list - update wording

--- src/views/JumpStartLists.vue
+++ src/views/JumpStartLists.vue
@@ -5,17 +5,31 @@
     </template>
     <template #content>
       <BaseTabs>
-        <BaseTab name="TODAY" selected="true">
+        <BaseTab name="Today" selected="true">
           <div v-for="(plan, index) in todayPlan" :key="index" class="cursor-pointer" @click="selectPlan(plan)">
             <JumpStartItem :model="plan" />
           </div>
-          <div v-if="!todayPlan.length" class="py-8">You don't have a plan for today</div>
+          <div v-if="!todayPlan.length" class="py-8">
+            <p>
+              There is nothing scheduled today.
+              <router-link class="text-secondary" :to="{ name: 'ArticleCategorySelection' }"
+                >Write some articles to start creating your Blue Delta</router-link
+              >.
+            </p>
+          </div>
         </BaseTab>
-        <BaseTab name="UPCOMING">
+        <BaseTab name="Upcoming">
           <div v-for="(plan, index) in upcomingPlan" :key="index" class="cursor-pointer" @click="selectPlan(plan)">
             <JumpStartItem :model="plan" />
           </div>
-          <div v-if="!upcomingPlan.length" class="py-8">You don't have a plan for upcoming</div>
+          <div v-if="!upcomingPlan.length" class="py-8">
+            <p>
+              There is nothing scheduled.
+              <router-link class="text-secondary" :to="{ name: 'ArticleCategorySelection' }"
+                >Write some articles to start creating your Blue Delta</router-link
+              >.
+            </p>
+          </div>
         </BaseTab>
       </BaseTabs>
     </template>
