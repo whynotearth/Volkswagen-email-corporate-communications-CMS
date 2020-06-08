ba18d9f363629a1a1c97220f7f9d07f14f266cac
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-06-08 23:54:29 +0430

    fix transition error in layout for group elements

--- src/views/ActivityFeedMemoItem.vue
+++ src/views/ActivityFeedMemoItem.vue
@@ -11,38 +11,40 @@
     </template>
 
     <template #content>
-      <div class="bg-background z-10 relative">
-        <div class="container px-0 md:px-6">
-          <div class="px-4 pb-4 pt-2">
-            <MemoListItem v-if="get(get_stat, `[${id}].memoStat`)" :model="get(get_stat, `[${id}].memoStat`)" />
+      <div>
+        <div class="bg-background z-10 relative">
+          <div class="container px-0 md:px-6">
+            <div class="px-4 pb-4 pt-2">
+              <MemoListItem v-if="get(get_stat, `[${id}].memoStat`)" :model="get(get_stat, `[${id}].memoStat`)" />
+            </div>
           </div>
         </div>
+        <BaseTabs>
+          <BaseTab class="text-left" name="Opened" :selected="true">
+            <div class="px-4 pt-4 bg-background">
+              <div class="mb-4" v-for="(readReportLog, index) in get(get_stat, `${id}.opened`, [])" :key="index">
+                <ActivityFeedReadReportLog
+                  :deliverDateTime="formatDate(readReportLog.deliverDateTime, 'dd MMM, yyyy h:mm aaa')"
+                  :openDateTime="formatDate(readReportLog.openDateTime, 'dd MMM, yyyy h:mm aaa')"
+                  :email="readReportLog.email"
+                />
+              </div>
+            </div>
+          </BaseTab>
+          <BaseTab class="text-left" name="Unread">
+            <div class="px-4 pt-4 bg-background">
+              <div class="mb-4" v-for="(readReportLog, index) in get(get_stat, `${id}.notOpened`, [])" :key="index">
+                <ActivityFeedReadReportLog
+                  :deliverDateTime="formatDate(readReportLog.deliverDateTime, 'dd MMM, yyyy h:mm aaa')"
+                  :openDateTime="formatDate(readReportLog.openDateTime, 'dd MMM, yyyy h:mm aaa')"
+                  :email="readReportLog.email"
+                  hideOpened="1"
+                />
+              </div>
+            </div>
+          </BaseTab>
+        </BaseTabs>
       </div>
-      <BaseTabs>
-        <BaseTab class="text-left" name="Opened" :selected="true">
-          <div class="px-4 pt-4 bg-background">
-            <div class="mb-4" v-for="(readReportLog, index) in get(get_stat, `${id}.opened`, [])" :key="index">
-              <ActivityFeedReadReportLog
-                :deliverDateTime="formatDate(readReportLog.deliverDateTime, 'dd MMM, yyyy h:mm aaa')"
-                :openDateTime="formatDate(readReportLog.openDateTime, 'dd MMM, yyyy h:mm aaa')"
-                :email="readReportLog.email"
-              />
-            </div>
-          </div>
-        </BaseTab>
-        <BaseTab class="text-left" name="Unread">
-          <div class="px-4 pt-4 bg-background">
-            <div class="mb-4" v-for="(readReportLog, index) in get(get_stat, `${id}.notOpened`, [])" :key="index">
-              <ActivityFeedReadReportLog
-                :deliverDateTime="formatDate(readReportLog.deliverDateTime, 'dd MMM, yyyy h:mm aaa')"
-                :openDateTime="formatDate(readReportLog.openDateTime, 'dd MMM, yyyy h:mm aaa')"
-                :email="readReportLog.email"
-                hideOpened="1"
-              />
-            </div>
-          </div>
-        </BaseTab>
-      </BaseTabs>
     </template>
     <template #footer>
       <NavigationBottom />
