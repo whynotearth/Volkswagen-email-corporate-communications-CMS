86350f91d64bd258d90705641a9049efce21e8f1
Somayeh Ebrahimi <s.ebrahimi@farakav.com>
2020-06-16 21:56:20 +0430

    #381 add heatmap chart to stats overview memo page

---
+++ src/components/BaseHeatmapChart.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <ApexCharts type="heatmap" height="350" :options="options" :series="series"></ApexCharts>
+  </div>
+</template>
+
+<script>
+import ApexCharts from 'vue-apexcharts';
+
+export default {
+  name: 'BaseHeatmapChart',
+  components: { ApexCharts },
+  props: {
+    options: {
+      type: Object,
+      required: true
+    },
+    series: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+.apexcharts-legend-series {
+  position: relative;
+  margin: 6px 0px !important;
+}
+.apexcharts-legend-text {
+  position: absolute !important;
+  top: 14px;
+  right: 3px;
+}
+.apexcharts-legend-marker {
+  margin-right: 4px;
+}
+.apexcharts-legend {
+  overflow: hidden !important;
+  padding: 10px !important;
+  flex-wrap: unset !important;
+}
+</style>
--- src/views/StatsOverviewMemos.vue
+++ src/views/StatsOverviewMemos.vue
@@ -42,6 +42,10 @@
           </div>
         </div>
 
+        <div class="container px-5 md:px-6 text-left mb-6">
+          <BaseHeatmapChart :options="options" :series="series" />
+        </div>
+
         <div class="container px-4 md:px-6 text-left pb-6">
           <div class="mb-6">
             <!-- link button -->
@@ -71,6 +75,7 @@ import LayoutFixedFooter from '@/components/LayoutFixedFooter';
 import NavigationBottom from '@/components/BaseNavigationBottom';
 import BaseAppBarHeader from '@/components/BaseAppBarHeader.vue';
 import BaseDropdown from '@/components/BaseDropdown';
+import BaseHeatmapChart from '@/components/BaseHeatmapChart';
 import ChartsStatsOverview from '@/components/ChartsStatsOverview';
 import BaseButtonPro from '@/components/BaseButtonPro';
 import Calendar from '@/assets/calendar.svg';
@@ -90,10 +95,294 @@ export default {
     NavigationBottom,
     LayoutFixedFooter,
     BaseDropdown,
+    BaseHeatmapChart,
     ChartsStatsOverview,
     BaseButtonPro,
     Calendar,
     Stat
+  },
+  data() {
+    return {
+      options: {
+        chart: {
+          height: 350,
+          type: 'heatmap',
+          toolbar: {
+            show: false
+          },
+          zoom: {
+            enabled: false
+          },
+          selection: {
+            enabled: false
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        colors: ['#03B3F9'],
+        stroke: {
+          colors: ['#ffffff'],
+          width: 8
+        },
+        fill: {
+          opacity: 1
+        },
+        xaxis: {
+          type: 'datetime',
+          crosshairs: {
+            show: false
+          },
+          labels: {
+            rotate: 0,
+            format: 'ddd',
+            hideOverlappingLabels: false,
+            offsetX: -16,
+            offsetY: 0,
+            style: {
+              color: '#757575',
+              fontFamily: 'VWText-Regular'
+            }
+          },
+          axisBorder: {
+            show: false
+          },
+          axisTicks: {
+            show: false
+          }
+        },
+        yaxis: {
+          opposite: true,
+          labels: {
+            style: {
+              color: '#757575',
+              fontFamily: 'VWText-Regular'
+            }
+          }
+        },
+        tooltip: {
+          enabled: false
+        },
+        states: {
+          normal: {
+            filter: {
+              type: 'none'
+            }
+          },
+          hover: {
+            filter: {
+              type: 'none'
+            }
+          },
+          active: {
+            filter: {
+              type: 'none'
+            }
+          }
+        },
+        plotOptions: {
+          heatmap: {
+            radius: 0,
+            enableShades: false,
+            shadeIntensity: 1,
+            colorScale: {
+              ranges: [
+                {
+                  from: 0,
+                  to: 1,
+                  color: '#EDEDED'
+                },
+                {
+                  from: 1,
+                  to: 15,
+                  color: '#D9EFFF'
+                },
+                {
+                  from: 15,
+                  to: 30,
+                  color: '#9EDEFD'
+                },
+                {
+                  from: 30,
+                  to: 45,
+                  color: '#71D2FC'
+                },
+                {
+                  from: 45,
+                  to: 60,
+                  color: '#3EC3FA'
+                },
+                {
+                  from: 60,
+                  to: 75,
+                  color: '#03B3F9'
+                }
+              ]
+            }
+          }
+        },
+        grid: {
+          show: false
+        },
+        legend: {
+          position: 'bottom',
+          markers: {
+            width: 40,
+            height: 10,
+            radius: 0
+          },
+          formatter: function(label, opts) {
+            return opts.w.config.plotOptions.heatmap.colorScale.ranges[opts.seriesIndex].to;
+          }
+        }
+      },
+      series: [
+        {
+          name: '10pm',
+          data: [
+            [1591488000000, 10], // new Date('2020-06-07').getTime()
+            [1591574400000, 0], // new Date('2020-06-08').getTime()
+            [1591660800000, 50], // new Date('2020-06-09').getTime()
+            [1591833600000, 10], // new Date('2020-06-10').getTime()
+            [1591920000000, 0], // new Date('2020-06-11').getTime()
+            [1592006400000, 50], // new Date('2020-06-12').getTime()
+            [1592092800000, 75] // new Date('2020-06-13').getTime()
+          ]
+        },
+        {
+          name: '8pm',
+          data: [
+            [1591488000000, 10],
+            [1591660800000, 10],
+            [1591747200000, 50],
+            [1591833600000, 70],
+            [1591920000000, 10],
+            [1592006400000, 0],
+            [1592092800000, 75]
+          ]
+        },
+        {
+          name: '6pm',
+          data: [
+            [1591574400000, 70],
+            [1591660800000, 0],
+            [1591747200000, 60],
+            [1591833600000, 0],
+            [1591920000000, 20],
+            [1592006400000, 0],
+            [1592092800000, 30]
+          ]
+        },
+        {
+          name: '4pm',
+          data: [
+            [1591574400000, 50],
+            [1591660800000, 7],
+            [1591747200000, 40],
+            [1591833600000, 18],
+            [1591920000000, 0],
+            [1592006400000, 32],
+            [1592092800000, 0]
+          ]
+        },
+        {
+          name: '2pm',
+          data: [
+            [1591574400000, 5],
+            [1591660800000, 65],
+            [1591747200000, 0],
+            [1591833600000, 16],
+            [1591920000000, 77],
+            [1592006400000, 0],
+            [1592092800000, 0]
+          ]
+        },
+        {
+          name: '12pm',
+          data: [
+            [1591574400000, 25],
+            [1591660800000, 0],
+            [1591747200000, 5],
+            [1591833600000, 0],
+            [1591920000000, 7],
+            [1592006400000, 0],
+            [1592092800000, 0]
+          ]
+        },
+        {
+          name: '10am',
+          data: [
+            [1591574400000, 0],
+            [1591660800000, 8],
+            [1591747200000, 14],
+            [1591833600000, 0],
+            [1591920000000, 0],
+            [1592006400000, 0],
+            [1592092800000, 40]
+          ]
+        },
+        {
+          name: '8am',
+          data: [
+            [1591574400000, 0],
+            [1591660800000, 55],
+            [1591747200000, 0],
+            [1591833600000, 0],
+            [1591920000000, 13],
+            [1592006400000, 0],
+            [1592092800000, 0]
+          ]
+        },
+        {
+          name: '6am',
+          data: [
+            [1591574400000, 0],
+            [1591660800000, 5],
+            [1591747200000, 0],
+            [1591833600000, 0],
+            [1591920000000, 13],
+            [1592006400000, 0],
+            [1592092800000, 70]
+          ]
+        },
+        {
+          name: '4am',
+          data: [
+            [1591574400000, 0],
+            [1591660800000, 5],
+            [1591747200000, 0],
+            [1591833600000, 0],
+            [1591920000000, 13],
+            [1592006400000, 0],
+            [1592092800000, 0]
+          ]
+        },
+        {
+          name: '2am',
+          data: [
+            [1591574400000, 30],
+            [1591660800000, 5],
+            [1591747200000, 0],
+            [1591833600000, 0],
+            [1591920000000, 73],
+            [1592006400000, 0],
+            [1592092800000, 40]
+          ]
+        },
+        {
+          name: '12am',
+          data: [
+            [1591574400000, 0],
+            [1591660800000, 5],
+            [1591747200000, 0],
+            [1591833600000, 0],
+            [1591920000000, 13],
+            [1592006400000, 0],
+            [1592092800000, 50]
+          ]
+        }
+      ]
+    };
   },
   created() {
     this.fetchStatsOverview();
