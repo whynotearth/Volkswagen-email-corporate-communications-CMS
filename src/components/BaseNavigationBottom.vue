702edd48e1d0f7e63b136f5ab1b51fed61c15f7b
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-06-19 17:31:04 +0430

    update bottom nav and home

--- src/components/BaseNavigationBottom.vue
+++ src/components/BaseNavigationBottom.vue
@@ -16,12 +16,12 @@
                 Home
               </router-link>
             </li>
-            <li class="text-white inline-block m-2 mx-3 md:mx-5 mb-1 text-xs flex-shrink-0">
+            <!-- <li class="text-white inline-block m-2 mx-3 md:mx-5 mb-1 text-xs flex-shrink-0">
               <router-link :active-class="'router-active-link'" class="router-normal-link" :to="{ name: 'Stats' }">
                 <StatsIcon class="m-auto" />
                 Stats
               </router-link>
-            </li>
+            </li> -->
             <!-- <li class="text-white inline-block m-2 mx-3 md:mx-5 mb-1 text-xs flex-shrink-0">
               <router-link
                 :active-class="'router-active-link'"
@@ -90,7 +90,7 @@ import MemoIcon from '@/assets/memo.svg';
 // import MemoStatslIcon from '@/assets/memo-stats.svg';
 // import ArticleIcon from '@/assets/article.svg';
 import BookingIcon from '@/assets/booking.svg';
-import StatsIcon from '@/assets/stats.svg';
+// import StatsIcon from '@/assets/stats.svg';
 import IconBottomNavFabOutcircle from '@/assets/bottom-nav-fab-outcircle.svg';
 
 export default {
@@ -102,7 +102,7 @@ export default {
     // MemoStatslIcon,
     // ArticleIcon,
     // BookingIcon,
-    StatsIcon,
+    // StatsIcon,
     IconBottomNavFabOutcircle
   },
   data() {
